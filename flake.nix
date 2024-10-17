33a76e9dc4d65eda5f7ce8c5840700f0006ed677
rybaz <>
2024-10-17 17:06:59 -0400

    adding pass

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    # nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
@@ -67,7 +67,7 @@
         mpv
         neovim          # god mode
         oh-my-posh      # zsh prompt
-        pass
+        pass.withExtensions (exts: [ exts.pass-otp ])
         ripgrep
         sox             # audio utils
         stow            # dotfile management
